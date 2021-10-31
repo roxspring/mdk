9137d6da22a271379dea873e0a549bf594e702f7
Rob Oxspring <roxspring@imapmail.org>
2021-10-31 11:23:04 +0000

    Gradle project framework

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    kotlin("jvm") version "1.5.31"
+
+    application
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("bom"))
+    implementation(kotlin("stdlib-jdk8"))
+
+    testImplementation(kotlin("test"))
+    testImplementation(kotlin("test-junit"))
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(8))
+}
+
+application {
+    mainClass.set("net.oxspring.mdk.cli.AppKt")
+}
---
+++ src/main/kotlin/net/oxspring/mdk/cli/App.kt
@@ -0,0 +1,5 @@
+package net.oxspring.mdk.cli
+
+fun main() {
+    println("Hello World!")
+}
